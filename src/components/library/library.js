e3e7c0c8c82fbe6933620f8a08b67058a60726f7
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-07-03 13:50:18 -0600

    implemented mapStateToProps and mapped over data to return the amount of course component instances we require

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions';
 
 import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
+
+    comonentWillMount() {
+        this.props.fetchCourses()
+    }
     render() {
+        console.log(this.props.courses);
         return (
             <div className="library">
                 <h1 className="library__title">Course Library</h1>
@@ -15,4 +23,10 @@ class Library extends Component {
     }
 }
 
-export default Library;
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps, actions)(Library);
