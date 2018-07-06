dfd14ca4ebb7e9255863658d0ee7ed5e2f6d0e06
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-07-06 11:12:54 -0600

    fixed bugg

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -7,22 +7,23 @@ import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
 
-    comonentWillMount() {
+    componentWillMount() {
         this.props.fetchCourses()
     }
 
     renderCourses() {
         const data = this.props.courses
-        
+
         return data.map((course, index) => {
             return <LibraryCourse {...course} key={index}/>
         })
     }
+
     render() {
         return (
             <div className="library">
                 <h1 className="library__title">Course Library</h1>
-                {this.renderCourses()}
+                { this.renderCourses() }
             </div>
         )
     }
