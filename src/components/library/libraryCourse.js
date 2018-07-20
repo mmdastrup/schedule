0a520952f30cd7f78bf1d2b09a400d699ff30d81
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-07-20 11:24:19 -0600

    used react animate height library to animate paragraph height

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,62 +1,65 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 import Icon from "../icon";
 import Arrow from "../arrow";
 import Action from "../action";
 
+import AnimateHeight from "react-animate-height";
+
 class LibraryCourse extends Component {
+  constructor(props) {
+    super(props);
 
-constructor(props) {
-  super(props)
-
-  this.state = {
-    status: true
+    this.state = {
+      status: true,
+      height: 0
+    };
   }
-}
-
-  renderDescription = function () {
-    if (!this.state.status) {
-      return (
-        <div className="library-course__description">
-          <label>Course Description</label>
-          <p>
-            {this.props.description}
-          </p>
-        </div>
-      )
-    } else {
-    }
-  }.bind(this);
 
   handleCallback = function(status) {
-    if(!status) {
-      document.getElementById('library-course').classList.add('library-course-selected');
+    let height = this.state.height == 0 ? 80 : 0;
+    if (!status) {
+      document.getElementById(this.id).classList.add("library-course-selected");
     } else {
-      document.getElementById('library-course').classList.remove('library-course-selected');
+      document.getElementById(this.id).classList.remove("library-course-selected");
     }
-    this.setState({ status })
-
+    this.setState({ 
+      status,
+      height
+    });
   }.bind(this);
 
   render() {
+    this.id = `library-course-${this.props.id}`;
     return (
-      <div id="library-course" className="library-course">
+      <div id={this.id} className="library-course">
         <div className="library-course__title-check">
           <label className="library-course__title">{this.props.title}</label>
           {Icon("fas fa-check", "library-course__icon")}
         </div>
-        <div className="library-course__line"></div>
-        <Arrow 
-        callback={status => this.handleCallback(status)}
-         id={this.props.id}
-          className="library-course__arrow" />
-        <Action 
-        id={this.props.id}
-        onClick={() => this.props.toggleEnrolled(this.props.id)} 
-        className="library-course__action" />
-        {this.renderDescription()}
+        <div className="library-course__line" />
+        <Arrow
+          callback={status => this.handleCallback(status)}
+          id={this.props.id}
+          className="library-course__arrow"
+        />
+        <Action
+          id={this.props.id}
+          onClick={() => this.props.toggleEnrolled(this.props.id)}
+          className="library-course__action"
+        />
+
+        <AnimateHeight
+          duration={300}
+          height={this.state.height}
+        >
+          <div className="library-course__description">
+            <label>Course Description</label>
+            <p>{this.props.description}</p>
+          </div>
+        </AnimateHeight>
       </div>
     );
   }
